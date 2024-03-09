08baea52347182cfbbdbcdc25a0a0c3e6ef7eab2
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2024-03-08 20:15:16 -0700

    Create Every_Exception.cpp

---
+++ Every_Exception.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<thread>
+
+using namespace std;
+
+class check
+{
+    private:
+        int amount;
+        int price;
+        
+    public:
+     void except(int p, int a)
+     {
+         amount= a;
+         price=p;
+         
+         if(price>amount)
+         {
+             throw 101;  // when you have written throw, there is no need to write return! 
+         }
+     }
+};
+
+
+int main()
+{
+    check obj1;
+    
+    try
+    {
+    obj1.except(9,2); //when you know that you are faaing an error, or an exception, you must use eception handling!
+    }
+    catch (...)
+    {
+        cout<<"exception msg";
+    }
+    
+    return 0;
+}
