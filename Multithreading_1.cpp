8625760c6ee31a6c107b29a6ee3c5221590f7468
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2024-03-23 11:44:13 -0600

    Create Multithreading_1.cpp

---
+++ Multithreading_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<thread>
+#include<chrono>
+
+using namespace std;
+
+void run(int count)
+{
+    while(count-->0)
+    {
+        cout<<"Multi-Threading"<<endl;
+    }
+    std::this_thread::sleep_for(chrono::seconds(2));
+}
+
+int main ()
+{ 
+    std::thread t1(run, 10);
+    cout<<"main()"<< endl;
+    t1.join();
+    cout<<"End here";
+    return 0;
+}
